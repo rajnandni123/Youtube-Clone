e80a2af4643920a9a0ee57c8ff75d81fe894fa97
rajnandni123 <vedicvanshi@gmail.com>
2024-07-22 10:22:36 +0530

    Initial Commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+  return (
+    <nav class="flex-div">
+      <div class="nav left flex-div">
+        <MenuIcon />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
